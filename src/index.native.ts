81e206bf638997e55ae9a99d01dff6b87bed679b
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2024-04-26 15:36:17 +0200

    chore: set patch version

--- src/index.native.ts
+++ src/index.native.ts
@@ -1,4 +1,4 @@
-// @deno-types="npm:@types/react@^18.2"
+// @deno-types="npm:@types/react@^18.2.0"
 import { useLayoutEffect } from 'react'
 
 export default useLayoutEffect
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-// @deno-types="npm:@types/react@^18.2"
+// @deno-types="npm:@types/react@^18.2.0"
 import { useEffect, useLayoutEffect } from 'react'
 
 export default typeof document !== 'undefined' ? useLayoutEffect : useEffect
